ceff02e8bef89323ffaca1f1e765df945248ac22
unknown <biruktawitaberaa@gmail.com>
2025-05-15 17:34:05 +0300

    fix the path

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
+import { Checkbox } from "../components/ui/checkbox";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "../components/ui/card";
 
 type SignInPageProps = {
   onLogin: () => void;
