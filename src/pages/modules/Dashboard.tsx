457541c9db675a33076da8a230a1138497b406fc
nathnael getachew <nathnaelgetachew12f@gmail.com>
2025-07-03 11:21:42 +0300

    Modified: foldername dashboard to HR in components

--- src/pages/modules/Dashboard.tsx
+++ src/pages/modules/Dashboard.tsx
@@ -1,14 +1,14 @@
 import { Button } from '../../components/ui/button'
-import StatsCards from '../../components/dashboard/StatsCards'
-import UpcomingEvents from '../../components/dashboard/UpcomingEvents'
-import RecentActivity from '../../components/dashboard/RecentActivity'
-import PendingActivity from '../../components/dashboard/PendingActivity'
+import StatsCards from '../../components/hr/StatsCards'
+import UpcomingEvents from '../../components/hr/UpcomingEvents'
+import RecentActivity from '../../components/hr/RecentActivity'
+import PendingActivity from '../../components/hr/PendingActivity'
 import { dashboardMetrics, departments, employeeStatusData, timeOffRequests, attendanceApprovals, recentActivities, upcomingEvents } from '../../data/data'
 import { motion } from 'framer-motion';
 import { Plus, RefreshCw, FileDown } from 'lucide-react';
 // import EmployeeStatus from '../components/dashboard/EmployeeStatus'
-import DepartmentChart from '../../components/dashboard/DepartmentChart'
-import EmployeeStatusChart from '../../components/dashboard/EmployeeChart'
+import DepartmentChart from '../../components/hr/DepartmentChart'
+import EmployeeStatusChart from '../../components/hr/EmployeeChart'
 
 
 function Dashboard() {
