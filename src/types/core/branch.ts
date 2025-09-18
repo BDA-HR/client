b2a06a491a3af8a10d96460fb2c21162f4b48238
nathnael getachew <nathnaelgetachew12f@gmail.com>
2025-09-18 13:33:22 +0300

    Modified: Core types of from string to UUID

--- src/types/core/branch.ts
+++ src/types/core/branch.ts
@@ -1,6 +1,7 @@
+import type { UUID } from 'crypto';
 import type { BaseDto } from './BaseDto';
 
-export type UUID = string;
+export type { UUID };
 
 export interface BranchListDto extends BaseDto {
   name: string;
--- src/types/core/fisc.ts
+++ src/types/core/fisc.ts
@@ -1,6 +1,8 @@
+import type { UUID } from 'crypto';
 import type { BaseDto } from './BaseDto';
 
-export type UUID = string;
+export type { UUID };
+
 
 export interface FiscYearListDto extends BaseDto {
   name: string;
