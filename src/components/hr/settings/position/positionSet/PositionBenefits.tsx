25995f387c448bc76c1bf803234ce18136f33bac
nathnael getachew <nathnaelgetachew12f@gmail.com>
2025-10-22 14:18:06 +0300

    Modified: position benefits to 4 grids in lg screens

--- src/components/hr/settings/position/positionSet/PositionBenefits.tsx
+++ src/components/hr/settings/position/positionSet/PositionBenefits.tsx
@@ -105,7 +105,7 @@ const PositionBenefits = forwardRef<PositionBenefitsRef, PositionBenefitsProps>(
         <div
           className={
             viewMode === "grid"
-              ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+              ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
               : "space-y-4"
           }
         >
